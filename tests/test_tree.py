b2d41cce99142a458d2abd6e471cc997651d3468
Scott Sadler <scott@alphanode.co>
2015-06-11 21:29:55 +0200

    more tree tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -28,11 +28,18 @@ class TestTree(unittest.TestCase):
 
     def test_init_empty_tree(self):
         tree = gitapp.tree.Tree(self.repo)
-        self.assertEqual(str(tree.oid),
-                         '4b825dc642cb6eb9a060e54bf8d69288fbee4904')
+        self.assertEqual('4b825dc642cb6eb9a060e54bf8d69288fbee4904',
+                         str(tree.oid))
+
+    def test_tree_set(self):
+        tree = gitapp.tree.Tree(self.repo).set('a', 'b')
+        self.assertEqual('00264970afd30838f2a3b17376e05f8ca03b658b',
+                         str(tree.oid))
+
 
     def test_init_some_tree(self):
-        tree = gitapp.tree.Tree(self.repo)
-        tree.set('a', 'a')
-        self.assertEqual(tree.oid, gitapp.tree.Tree(self.repo, tree._tree).oid)
+        tree = gitapp.tree.Tree(self.repo).set('a', 'a')
+        newtree = gitapp.tree.Tree(self.repo, tree._tree)
+        self.assertEqual('b39954843ff6e09ec3aa2b942938c30c6bd1629e',
+                         str(newtree.oid))
 
